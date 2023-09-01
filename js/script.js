f095656094d72b11640f1bd12056f94336a41d06
Ranjan Kumar <111798933+vtu16964ranjankumar@users.noreply.github.com>
2023-09-02 00:50:47 +0530

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// ================= typing Animation ===========================
-var typed = new typed(".typing", {
-    strings: ["", "Web Develepor", "Student", "Graphic Designer", "YouTuber"],
-    typeSpeed: 100,
-    BackSpeed: 60,
-    loop: true
-
-})
